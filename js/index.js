d2740b614a2a29e65dc6760e00afb723df6a8ae9
Douglas Montes <douglasmontes.dev@gmail.com>
2022-01-20 16:57:51 -0300

    added dynamic text to submit button

--- js/index.js
+++ js/index.js
@@ -1,14 +1,30 @@
 
+var messageForm = document.getElementById('message-form');
 var cypherField = document.getElementById('cypher-and-increment');
 var cypherSelect = document.getElementById('cypher-select');
 
 var increment = document.createElement('input');
 increment.type = 'text';
 increment.setAttribute('id', 'increment');
+
 var incrementLabel = document.createElement('label');
 incrementLabel.setAttribute('for', 'increment');
 incrementLabel.innerText = 'Informe o incremento a ser utilizado';
+
 var linebreak = document.createElement('br');
+
+var codeRadio = document.getElementById('code-radio');
+var decodeRadio = document.getElementById('decode-radio');
+
+var submitButton = document.getElementById('submit-button');
+
+codeRadio.addEventListener('click', () => {
+    submitButton.innerText = 'codificar mensagem';
+});
+
+decodeRadio.addEventListener('click', () => {
+    submitButton.innerText = 'decodificar mensagem';
+});
 
 cypherSelect.addEventListener('change', () => {
     if (cypherSelect.value == 'cesar') {
