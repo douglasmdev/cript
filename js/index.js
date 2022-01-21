5e0afdc17be17a669679a8a313ce464f4a221aae
Douglas Montes <douglasmontes.dev@gmail.com>
2022-01-21 13:04:49 -0300

    created initial column design

--- js/index.js
+++ js/index.js
@@ -6,10 +6,11 @@ var cypherSelect = document.getElementById('cypher-select');
 var increment = document.createElement('input');
 increment.type = 'text';
 increment.setAttribute('id', 'increment');
+increment.setAttribute('class', 'wide-text-input');
 
 var incrementLabel = document.createElement('label');
 incrementLabel.setAttribute('for', 'increment');
-incrementLabel.innerText = 'Informe o incremento a ser utilizado';
+incrementLabel.innerText = 'Informe o incremento a ser utilizado:';
 
 var linebreak = document.createElement('br');
 
@@ -18,12 +19,16 @@ var decodeRadio = document.getElementById('decode-radio');
 
 var submitButton = document.getElementById('submit-button');
 
+var resultTitle = document.getElementById('result-title');
+
 codeRadio.addEventListener('click', () => {
     submitButton.innerText = 'codificar mensagem';
+    resultTitle.innerText = 'Sua Mensagem Codificada:';
 });
 
 decodeRadio.addEventListener('click', () => {
     submitButton.innerText = 'decodificar mensagem';
+    resultTitle.innerText = 'Sua Mensagem Decodificada:';
 });
 
 cypherSelect.addEventListener('change', () => {
