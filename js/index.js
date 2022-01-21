466dfab86d39cc0c3a7dab31739de08dd4f729a4
Douglas Montes <douglasmontes.dev@gmail.com>
2022-01-21 13:46:56 -0300

    implemented base64 code-decode

--- js/index.js
+++ js/index.js
@@ -11,7 +11,9 @@ var submitButton = document.getElementById('submit-button');
 var resultTitle = document.getElementById('result-title');
 var resultField = document.getElementById('result-field');
 var linebreak = document.createElement('br');
-var result = ''
+var userInput = '';
+var action = 'code';
+var cypher = 'base64';
 
 increment.type = 'text';
 increment.setAttribute('id', 'increment');
@@ -24,11 +26,13 @@ incrementLabel.innerText = 'Informe o incremento a ser utilizado:';
 codeRadio.addEventListener('click', () => {
     submitButton.innerText = 'codificar mensagem';
     resultTitle.innerText = 'Sua Mensagem Codificada:';
+    action = 'code';
 });
 
 decodeRadio.addEventListener('click', () => {
     submitButton.innerText = 'decodificar mensagem';
     resultTitle.innerText = 'Sua Mensagem Decodificada:';
+    action = 'decode';
 });
 
 cypherSelect.addEventListener('change', () => {
@@ -36,22 +40,20 @@ cypherSelect.addEventListener('change', () => {
         cypherField.appendChild(incrementLabel);
         cypherField.appendChild(linebreak);
         cypherField.appendChild(increment);
+        cypher = 'cesar';
     } else {
         incrementLabel.remove();
         increment.remove();
+        cypher = 'base64';
     }
 });
 
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
-    result = getFormInput();
-    displayFormInput(result);
+    userInput = messageField.value;
+
+    if (action == 'code' && cypher == 'base64')
+        resultField.innerText = btoa(userInput);
+    else if (action == 'decode' && cypher == 'base64')
+        resultField.innerText = atob(userInput);
 });
-
-function getFormInput() {
-    return messageField.value;
-}
-
-function displayFormInput(str) {
-    resultField.innerHTML = `${str}`;
-}
